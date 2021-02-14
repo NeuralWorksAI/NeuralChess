8d1ab307f33b72b488a3c1bc5de5e46a57b9b998
00MB <s2061204@ed.ac.uk>
2021-02-14 21:56:54 +0000

    V0: Removed dataset, added minimax, stockfish.

--- V00/engine.py
+++ V00/engine.py
@@ -1,3 +1,18 @@
 import chess
+import chess.engine
 
 board = chess.Board()
+
+
+def minimax(node, depth, maxPlayer):
+    if depth = 0 or #terminal node:
+        return #herusitic value of node
+    if maximisingPlayer:
+        value = -1000
+        for child in node.legal_moves:
+            value = max(value, minimax(child, depth - 1, False))
+        return value
+    else:
+        value = 1000
+        for child in node.legal_moves:
+            value = min(value, minimax(child, depth, depth - 1, True))
