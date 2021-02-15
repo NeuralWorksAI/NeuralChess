d93ac8a9d2ebccfae82efbd39a3ba8b87007a520
00MB <s2061204@ed.ac.uk>
2021-02-15 04:16:56 +0000

    Working minimax algorithm

--- V00/engine.py
+++ V00/engine.py
@@ -1,17 +1,14 @@
 import chess
 import chess.engine
 import time
+import math
 
 start_time = time.time()
 #r1bq1rkR/1ppnbpp1/p3p3/4P3/4BB2/4P3/PPP2PP1/RN1QK3 w Qq - 1 1
-board = chess.Board("rnbqkbnr/p1pp1ppp/1p2p3/6B1/3P4/8/PPP1PPPP/RN1QKBNR w KQkq - 0 1")
+board = chess.Board()
 print(board)
 engine = chess.engine.SimpleEngine.popen_uci("/home/qqze/Documents/NeuralWorks/NeuralChess/NeuralChess/V00/stockfish_20090216_x64_bmi2")
 
-class Node:
-    def __init__(self, eval, position):
-        self.eval = eval
-        self.position = position
 
 #position = [board, evaluation]
 def moveToPosition(board, move):
@@ -19,31 +16,32 @@ def moveToPosition(board, move):
     childBoard.push_san(str(move))
     return childBoard
 
-def minimax(node, depth, maxPlayer, move=""):
+def minimax(node, depth, maxPlayer):
+
     if depth == 0 or node.is_game_over():
         evalDict = engine.analyse(node, chess.engine.Limit(depth=0))
-        return evalDict["score"].relative.score(mate_score=100000)
+        return None, evalDict["score"].relative.score(mate_score=100000)
 
     if maxPlayer:
-        value = -1000000
+        max_eval = -math.inf
         for child in node.legal_moves:
-            eval = minimax(moveToPosition(node, child), depth - 1, False, child)
-            value = max(value, eval)
-            print(f"max value: {value}")
-            print(depth)
-            print(child)
-        return value
+            eval = minimax(moveToPosition(node, child), depth - 1, False)[1]
+            if eval > max_eval:
+                max_eval = eval
+                best_move = child
+        return best_move, max_eval
 
     else:
-        value = 1000000
+        min_eval = math.inf
         for child in node.legal_moves:
-            eval = minimax(moveToPosition(node, child), depth - 1, True, child)
-            print(depth)
-            value = min(value, eval)
-        print(f"min value: {value}")
-        return value
+            eval = minimax(moveToPosition(node, child), depth - 1, True)[1]
+            if eval < min_eval:
+                min_eval = eval
+                best_move = child
+        return best_move, min_eval
 
 
-print(minimax(board, 3, True))
+best_move, best_eval = minimax(board, 2, True)
+print(f"best move: {best_move}, eval: {best_eval/100}")
 print(f"finished in {time.time() - start_time} seconds")
 print(board.legal_moves)
