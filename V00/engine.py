fb268e9ac2794d9fa8ac38ad2f636fec46d6bf9c
00MB <s2061204@ed.ac.uk>
2021-02-15 05:06:25 +0000

    Added alpha-beta pruning

--- V00/engine.py
+++ V00/engine.py
@@ -4,7 +4,7 @@ import time
 
 start_time = time.time()
 #r1bq1rkR/1ppnbpp1/p3p3/4P3/4BB2/4P3/PPP2PP1/RN1QK3 w Qq - 1 1
-board = chess.Board()
+board = chess.Board("rnbqkbnr/2pp1ppp/pp6/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 0 1")
 
 engine = chess.engine.SimpleEngine.popen_uci("/home/qqze/Documents/NeuralWorks/NeuralChess/NeuralChess/V00/stockfish_20090216_x64_bmi2")
 
@@ -15,32 +15,38 @@ def moveToPosition(board, move):
     childBoard.push_san(str(move))
     return childBoard
 
-def minimax(node, depth, maxPlayer):
+def minimax(node, depth, alpha, beta, maxPlayer):
 
     if depth == 0 or node.is_game_over():
         evalDict = engine.analyse(node, chess.engine.Limit(depth=0))
-        return None, evalDict["score"].relative.score(mate_score=100000)
+        return None, evalDict["score"].relative.score(mate_score=1000000)
 
     if maxPlayer:
         max_eval = -1000000
         for child in node.legal_moves:
-            eval = minimax(moveToPosition(node, child), depth - 1, False)[1]
+            eval = minimax(moveToPosition(node, child), depth - 1, alpha, beta, False)[1]
             if eval > max_eval:
                 max_eval = eval
                 best_move = child
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
         return best_move, max_eval
 
     else:
         min_eval = 1000000
         for child in node.legal_moves:
-            eval = minimax(moveToPosition(node, child), depth - 1, True)[1]
+            eval = minimax(moveToPosition(node, child), depth - 1, alpha, beta, True)[1]
             if eval < min_eval:
                 min_eval = eval
                 best_move = child
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
         return best_move, min_eval
 
 
-best_move, best_eval = minimax(board, 2, True)
+best_move, best_eval = minimax(board, 4, -1000000, 1000000, True)
 print(f"best move: {best_move}, eval: {best_eval/100}")
 print(f"finished in {time.time() - start_time} seconds")
 board.push(best_move)
